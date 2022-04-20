d8ef3348ddf167e9e6a1e8603866a1cec65c5f23
Isaac Uduehi <osayi4jesus@gmail.com>
2022-04-20 08:01:43 +0100

    Implicit Intent

---
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.implicitintent
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnUpload.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it,0)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK){
+            val uri = data?.data
+            imageView.setImageURI(uri)
+        }
+    }
+}
