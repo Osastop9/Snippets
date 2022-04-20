f31ee5ae04463eb0c67bf269a4c490016a985d14
Isaac Uduehi <osayi4jesus@gmail.com>
2022-04-20 08:11:08 +0100

    first commit

---
+++ app/src/main/java/com/example/intent/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        btnReturn.setOnClickListener {
+            finish()
+        }
+    }
+}
